d0beac8540404d5b23b61f295522afe7745c6de9
Sam Lee <sflee36@yahoo.com.au>
2021-07-18 16:16:55 +0800

    Add dependencies for dom router and create simple nav bar with routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/navbar/navbar";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Privacy from "./pages/privacy";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/privacy">
+            <Privacy />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
